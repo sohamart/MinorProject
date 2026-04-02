9ccddc36caf150ea2dab0a5b3639a5d6a471afca
soham dutta <sohamduttabwn@gmail.com>
2026-04-02 23:00:05 +0530

    add design mobile nav, home , login all

--- frontend/src/components/sidebar/StudentSidebar/SidebarMob.jsx
+++ frontend/src/components/sidebar/StudentSidebar/SidebarMob.jsx
@@ -56,7 +56,7 @@ const SidebarMob = () => {
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: "-100%", opacity: 0 }}
             transition={{ duration: 0.5, ease: "easeInOut" }}
-            className="p-12 fixed top-0 left-0  h-screen w-screen backdrop-blur-xl z-22"
+            className="p-12 fixed top-0 left-0  h-screen w-screen bg-black  z-22"
           >
 
             {/* 🔥 Cross Button (Animated) */}
