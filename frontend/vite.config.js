e80c9fd9c6d7487dc008cd0725fbffc5e979071c
soham dutta <sohamduttabwn@gmail.com>
2026-04-02 18:54:26 +0530

    add design mobile nav

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import fs from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    
+    // 👇 THIS IS THE FIX
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        fs.copyFileSync(
+          resolve(__dirname, 'public/_redirects'),
+          resolve(__dirname, 'dist/_redirects')
+        )
+      }
+    }
+  ],
+
   server: {
     host: true
-  
-}
+  }
 })
-
