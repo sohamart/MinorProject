14e8c8df58887d7130978253d3d7b43f651b6645
soham dutta <sohamduttabwn@gmail.com>
2026-04-29 11:13:35 +0530

    admin pannel student register section

--- frontend/src/pages/student/student.jsx
+++ frontend/src/pages/student/student.jsx
@@ -32,7 +32,7 @@ const Student = () => {
   return (
     <>
       <div
-        className="relative flex w-screen h-screen  overflow-hidden text-white ">
+        className="relative flex w-screen h-[90vh]  overflow-hidden text-white ">
 
 
        
