0ab09148a8614ca7542c5e7115dda6fdd6e90c5c
soham dutta <sohamduttabwn@gmail.com>
2026-05-09 20:53:01 +0530

    admin pannel student register section

--- frontend/src/components/loading/loading.jsx
+++ frontend/src/components/loading/loading.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ClockLoader } from 'react-spinners'
 
+const Loading = () => {
 
+  const [showMessage, setShowMessage] = useState(false)
 
-const loading = () => {
-  const localdate = new Date()
-  console.log(localdate)
+  useEffect(() => {
+
+    // 4 sec pore message show hobe
+    const timer = setTimeout(() => {
+      setShowMessage(true)
+    }, 4000)
+
+    return () => clearTimeout(timer)
+
+  }, [])
+
   return (
-    <div className='transform  transition ease-in duration-300 flex-col h-screen w-screen flex bg-black  items-center justify-center '>
-      <h1 className='text-white text-xl lg:text-4xl md:text-4xl font-bold absolute top-2 '>C.R TIME PRO</h1>
-      
+    <div className='transform transition ease-in duration-300 flex-col h-screen w-screen flex bg-black items-center justify-center relative'>
 
-      
+      <h1 className='text-white text-xl lg:text-4xl md:text-4xl font-bold absolute top-2'>
+        C.R TIME PRO
+      </h1>
+
       <ClockLoader
         color="#ffffff"
         loading
         size={80}
       />
 
-      
-      
+      {
+        showMessage && (
+          <p className='text-white mt-8 text-center px-4 text-sm lg:text-lg animate-pulse'>
+            Please wait... First time may take some time because the server is starting now.
+          </p>
+        )
+      }
+
     </div>
   )
 }
 
-export default loading
+export default Loading
