8c605a31c92adb1b0d9265c63e3e863a127f22bf
soham dutta <sohamduttabwn@gmail.com>
2026-05-20 19:18:18 +0530

    added onesignal worker

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -27,7 +27,9 @@ createRoot(document.getElementById('root')).render(
                     pauseOnFocusLoss
                     draggable
                     pauseOnHover
-                    theme="dark"
+                    theme="light"
+                    toastClassName="rounded-4xl"
+                    
                     
                 />
             </BrowserRouter>
