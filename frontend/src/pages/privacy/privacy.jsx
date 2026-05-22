34f8e4d82b327139555b7cd9d7807fb4a4a13f47
soham dutta <sohamduttabwn@gmail.com>
2026-05-22 13:40:37 +0530

    added onesignal worker

--- frontend/src/pages/privacy/privacy.jsx
+++ frontend/src/pages/privacy/privacy.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Privacy = () => {
   return (
-    <div className="min-h-screen overflow-auto no-scrollbar  py-10 px-5">
-      <div className="max-w-4xl mx-auto bg-black/20  border border-white/30 shadow-lg rounded-2xl p-8">
+    <div className="min-h-screen overflow-y-auto no-scrollbar  py-10 px-5">
+      <div className="max-w-4xl  mx-auto bg-black/20  border border-white/30 shadow-lg rounded-2xl p-8">
         
         <h1 className="text-4xl font-bold text-center text-blue-700 mb-4">
           Privacy Policy
