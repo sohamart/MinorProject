2862e670e70b32dac5d670619b16d0a9bc10450b
soham dutta <sohamduttabwn@gmail.com>
2026-05-22 13:54:12 +0530

    added onesignal worker

--- frontend/src/pages/privacy/privacy.jsx
+++ frontend/src/pages/privacy/privacy.jsx
@@ -12,7 +12,7 @@ const Privacy = () => {
       <div className="max-w-4xl relative mx-auto bg-black/20  border border-white/30 shadow-lg rounded-2xl p-8">
         <div className='absolute z-12 w-10 h-10  flex justify-center items-center  bg-white/20 border-white/50  border-b-2 rounded-b-2xl top-0 right-2'>
                      <House
-                     onclick={home}
+                     onClick={home}
                      color="#ffffff" />
       </div>
         <h1 className="text-4xl font-bold text-center text-blue-700 mb-4">
