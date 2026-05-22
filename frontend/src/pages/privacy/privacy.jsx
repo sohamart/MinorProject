6785f2b3df324a5a1aa52fe83ccaa6ce8bbfa29b
soham dutta <sohamduttabwn@gmail.com>
2026-05-22 13:52:55 +0530

    added onesignal worker

--- frontend/src/pages/privacy/privacy.jsx
+++ frontend/src/pages/privacy/privacy.jsx
@@ -8,13 +8,13 @@ const Privacy = () => {
       }
   return (
     <div className="h-screen overflow-y-auto no-scrollbar py-10 px-5">
-      <div className='absolute z-12 w-10 h-10  flex justify-center items-center  bg-white/20 border-white/50  border-b-2 rounded-b-2xl top-0 right-4'>
+      
+      <div className="max-w-4xl relative mx-auto bg-black/20  border border-white/30 shadow-lg rounded-2xl p-8">
+        <div className='absolute z-12 w-10 h-10  flex justify-center items-center  bg-white/20 border-white/50  border-b-2 rounded-b-2xl top-0 right-2'>
                      <House
                      onclick={home}
                      color="#ffffff" />
       </div>
-      <div className="max-w-4xl  mx-auto bg-black/20  border border-white/30 shadow-lg rounded-2xl p-8">
-        
         <h1 className="text-4xl font-bold text-center text-blue-700 mb-4">
           Privacy Policy
         </h1>
